aea1d9115b559113b43f9c4128edb1646be9e7b4
LuisArturoM <119456723+LuisArturoM@users.noreply.github.com>
2025-01-17 22:52:34 -0600

    Tabla y DonutCharts
    Se agregó una tabla con info de los agentes y las donutcharts de cada agente y de todos los agentes.

---
+++ src/components/DonutChartAgente.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { AnyUseMutationOptions } from "@tanstack/react-query";
+import { DonutChart } from "./DonutChart"
+
+interface DonutChartHeroProps {
+    data: Array<{
+        name: string;
+        amount: number;
+      }>;
+    agentName: string;
+}
+
+
+export const DonutChartSolo = ({data, agentName}:DonutChartHeroProps) => (
+        <div className="flex flex-col items-center justify-center gap-4 border-sky-500 bshadow-md p-4 rounded-md border-2 m-5">
+      <p className="text-gray-700 dark:text-gray-300 mt-4 text-blue-700 font-bold border-b-2 border-gray-800 ">Agente {agentName}</p>
+      <DonutChart
+        data={data}
+        category="name"
+        value="amount"
+      />
+    </div>
+)
---
+++ src/lib/heroicons-react.d.ts
@@ -0,0 +1,10 @@
+declare module '@heroicons/react/outline' {
+    const content: { [key: string]: React.ComponentType<React.SVGProps<SVGSVGElement>> };
+    export default content;
+  }
+  
+  declare module '@heroicons/react/solid' {
+    const content: { [key: string]: React.ComponentType<React.SVGProps<SVGSVGElement>> };
+    export default content;
+  }
+  
