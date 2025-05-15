0054e790260ae0209a69e7022afdeb150e98d748
Christian <misa_936@hotmail.com>
2025-05-14 23:49:27 -0600

    fix styles

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,9 @@
-import { FC, ReactNode, useRef, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './Layout.module.css';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { FC, ReactNode } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./Layout.module.css";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 interface LayoutProps {
   children: ReactNode; // El contenido que se renderizará dentro del layout
@@ -15,61 +15,104 @@ const click = () => {
 };
 
 const Layout: FC<LayoutProps> = ({ children, onLogout }) => {
-  
   return (
     <div className={` ${styles.layout}`}>
-      
-       <nav className="w-full navbar navbar-expand-lg navbar-dark bg-dark px-4">
-        <a className={`navbar-brand ${styles.brand}`} href="/">Proyecto M</a>
+      <nav className="w-full navbar navbar-expand-lg navbar-dark bg-dark px-4 sticky-top">
+        <a className={`navbar-brand ${styles.brand}`} href="/">
+          Proyecto M
+        </a>
         <div className="navbar-collapse justify-content-end p-2">
           <ul className="navbar-nav">
             <li className="nav-item w-25">
-              <a className={`nav-link p-2 w-50 ${styles.nav_link}`} onClick={click}>Home</a>
+              <a
+                className={`nav-link p-2 w-50 ${styles.nav_link}`}
+                onClick={click}
+              >
+                Home
+              </a>
             </li>
             <li className="nav-item w-25">
-              <a className={`nav-link p-2 w-50 ${styles.nav_link}`} onClick={click}>About</a>
+              <a
+                className={`nav-link p-2 w-50 ${styles.nav_link}`}
+                onClick={click}
+              >
+                About
+              </a>
             </li>
             <li className="nav-item w-25">
-              <a className={`nav-link p-2 w-50 ${styles.nav_link}`} onClick={click}>Services</a>
+              <a
+                className={`nav-link p-2 w-50 ${styles.nav_link}`}
+                onClick={click}
+              >
+                Services
+              </a>
             </li>
             <li className="nav-item w-25">
-              <a className={`nav-link p-2 w-50 ${styles.nav_link}`} onClick={click}>Contact</a>
+              <a
+                className={`nav-link p-2 w-50 ${styles.nav_link}`}
+                onClick={click}
+              >
+                Contact
+              </a>
             </li>
             <li className="nav-item w-25">
-              <a className={`nav-link p-2 w-50 ${styles.nav_link}`} onClick={onLogout}>Logout</a>
+              <a
+                className={`nav-link p-2 w-50 ${styles.nav_link}`}
+                onClick={onLogout}
+              >
+                Logout
+              </a>
             </li>
           </ul>
         </div>
-      </nav> 
+      </nav>
 
-      <div className="d-flex h-full">
+      <div className="d-flex h-full ">
         {/* <div className="flex-shrink-0 bg-dark text-white h-screen" style={{ width: '250px' }}> */}
-        <div className={`flex-shrink-0 bg-dark text-white h-screen col-lg-4 col-sm-2 ${styles.sidebar}`} style={{ width: '250px' }}>
+        <div
+          className={`flex-shrink-0 bg-dark text-white h-screen col-lg-4 col-sm-2 sticky-top ${styles.sidebar}`}
+          style={{ width: "250px" }}
+        >
           {/* <p className="fs-2">Administración</p> */}
-          <div className="nav flex-column">
-            <Link to="/dashboard" className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}>
+          <div className="nav flex-column sticky-top">
+            <Link
+              to="/dashboard"
+              className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}
+            >
               Dashboard
             </Link>
-            <Link to="/agents" className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}>
+            <Link
+              to="/agents"
+              className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}
+            >
               Actividad
             </Link>
-            <Link to="/management" className={`w-75 nav-link text-white px-3 py-2 ${styles.side_options}`}>
+            <Link
+              to="/management"
+              className={`w-75 nav-link text-white px-3 py-2 ${styles.side_options}`}
+            >
               Estadisticas
             </Link>
-            <Link to="/tools" className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}>
+            <Link
+              to="/tools"
+              className={`w-50 nav-link text-white px-3 py-2 ${styles.side_options}`}
+            >
               Analisis
             </Link>
-            <Link to="/settings" className={`w-75 nav-link text-white px-3 py-2 ${styles.side_options}`}>
+            <Link
+              to="/settings"
+              className={`w-75 nav-link text-white px-3 py-2 ${styles.side_options}`}
+            >
               Configuración
             </Link>
           </div>
-
-          
         </div>
-      
 
         <div className={`mb-5 ${styles.content}`}>
-          <div className="col-lg-11 col-md-10 col-sm-8 m-3 p-2 bg-light border border-rounded" style={{ borderRadius: '15px' }}>
+          <div
+            className="col-lg-11 col-md-10 col-sm-8 m-3 p-2 bg-light border border-rounded"
+            style={{ borderRadius: "15px" }}
+          >
             {/* hola */}
             {children}
           </div>
@@ -81,14 +124,12 @@ const Layout: FC<LayoutProps> = ({ children, onLogout }) => {
         </div> */}
       </div>
 
-      
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" defer></script>
-
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        defer
+      ></script>
     </div>
-
-    
   );
 };
-
 
 export default Layout;
