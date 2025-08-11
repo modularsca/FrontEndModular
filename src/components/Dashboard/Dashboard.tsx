3aea3adf765f541e408abbc8581dba75aa8b8cd6
Christian <misa_936@hotmail.com>
2025-08-10 20:03:20 -0600

    fix

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -4,18 +4,9 @@ import AgentesDonuts from "../Views/AgentesDonuts/AgentesDonuts";
 import { useState } from "react";
 
 const Dashboard = () => {
-  const [filter, setFilter] = useState("");
-
   return (
     <div className="dashboard-container vh-full bg-white p-3">
       <Title className=" text-3xl m-2">Dashboard</Title>
-      <input
-        type="text"
-        placeholder="Filtrar agentes..."
-        value={filter}
-        onChange={(e) => setFilter(e.target.value)}
-        className="p-2 border rounded mb-4 w-full"
-      />
       <Card>
         <AgentesDonuts />
       </Card>
